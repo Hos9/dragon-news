a3a350b4349977c70b25f067d8475bcd5d2d093d
Hos9 <shahin9315@gmail.com>
2026-04-27 05:42:57 +0600

    Navbar & Header components created

---
+++ src/components/shared/Header.jsx
@@ -0,0 +1,9 @@
+const Header = () => {
+  return (
+    <div className="container mx-auto">
+      <h2>This is Header</h2>
+    </div>
+  );
+};
+
+export default Header;
---
+++ src/components/shared/Navbar.jsx
@@ -0,0 +1,9 @@
+const Navbar = () => {
+  return (
+    <div className="container mx-auto">
+      <h2>This is Navbar</h2>
+    </div>
+  );
+};
+
+export default Navbar;
