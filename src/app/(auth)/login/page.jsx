cabfc74d1861f72fbfe44cfd7e43d769ef0f0954
Hos9 <shahin9315@gmail.com>
2026-04-27 05:56:10 +0600

    vercel route added

---
+++ src/app/(auth)/login/page.jsx
@@ -0,0 +1,9 @@
+const LoginPage = () => {
+  return (
+    <div className="container mx-auto">
+      <h2>Login Page</h2>
+    </div>
+  );
+};
+
+export default LoginPage;
---
+++ src/app/(auth)/register/page.jsx
@@ -0,0 +1,9 @@
+const RegisterPage = () => {
+  return (
+    <div className="container mx-auto">
+      <h2>Register Page</h2>
+    </div>
+  );
+};
+
+export default RegisterPage;
